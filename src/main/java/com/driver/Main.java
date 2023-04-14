433c418840366ccba5a7710409828b3b9e28cd82
rohit <rohitme0091@gmail.com>
2023-04-14 20:31:40 +0530

    checking for successfull run

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,27 @@ package com.driver;
 
 public class Main {
 
+     class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+         public int product(int x, int y, int z) {
+            return x*y*z;
+         }
+         public double product(double x, double y) {
+            return x*y;
+         }
+    }
+
+    public void main(String[] args) {
+
+        Product p = new Product();
+
+        p.product(1,2);
+
+        p.product(1,2,3);
+
+        p.product(1.0000,2.000);
+    }
+
 }
