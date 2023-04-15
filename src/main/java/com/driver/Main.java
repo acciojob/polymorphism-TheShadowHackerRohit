d492138f981d3e14f85ec4241bcf866b9954811c
rohit <rohitme0091@gmail.com>
2023-04-15 11:42:51 +0530

    done all task

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,21 +2,22 @@ package com.driver;
 
 public class Main {
 
-    static class  Product{
-        public int product(int x, int y) {
+    static class  Product{// task 1
+        public int product(int x, int y) {//task 3
             return x*y;
         }
-         public int product(int x, int y, int z) {
+
+         public int product(int x, int y, int z) {//task 4
             return x*y*z;
          }
-         public double product(double x, double y) {
+         public double product(double x, double y) {//task 5
             return x*y;
          }
     }
 
     public static void main(String[] args) {
 
-        Product p = new Product();
+        Product p = new Product();//task 2
 
         System.out.println(p.product(1,2));
 
